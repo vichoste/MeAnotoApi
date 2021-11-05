2e79e0e256c6c275e8bd6dacf7c36842a34b6eea
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-11-04 21:11:43 -0300

    Fix

--- Models/Entities/CampusSingular.cs
+++ Models/Entities/CampusSingular.cs
@@ -2,7 +2,7 @@
 using System.Text.Json.Serialization;
 
 namespace MeAnotoApi.Models.Entities {
-	public class CampusSingular {
+	public class CampusSingular : Entity{
 		public virtual int InstitutionId { get; set; }
 		[JsonIgnore]
 		public virtual Institution Institution { get; set; }
