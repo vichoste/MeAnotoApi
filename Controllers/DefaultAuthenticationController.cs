961f0ca89ad5697c7b24c6585ec85a76f88f485e
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-11-04 21:35:38 -0300

    Root

--- Controllers/DefaultAuthenticationController.cs
+++ Controllers/DefaultAuthenticationController.cs
@@ -6,7 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 using MeAnotoApi.Authentication;
-using MeAnotoApi.Models.Users;
 using MeAnotoApi.Models.Users.Default;
 
 using Microsoft.AspNetCore.Identity;
@@ -17,11 +16,11 @@ using Microsoft.IdentityModel.Tokens;
 namespace MeAnotoApi.Controllers {
 	[Route("Api/Authentication")]
 	[ApiController]
-	public class AuthenticateController : ControllerBase {
-		private readonly UserManager<User> _UserManager;
+	public class DefaultAuthenticationController : ControllerBase {
+		private readonly UserManager<DefaultUser> _UserManager;
 		private readonly RoleManager<IdentityRole> _RoleManager;
 		private readonly IConfiguration _Configuration;
-		public AuthenticateController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration) {
+		public DefaultAuthenticationController(UserManager<DefaultUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration) {
 			this._UserManager = userManager;
 			this._RoleManager = roleManager;
 			this._Configuration = configuration;
@@ -61,7 +60,7 @@ namespace MeAnotoApi.Controllers {
 			if (userExists != null) {
 				return this.Unauthorized();
 			}
-			User user = model.Role switch {
+			DefaultUser user = model.Role switch {
 				"Attendee" => new AttendeeUser() {
 					Email = model.Email,
 					SecurityStamp = Guid.NewGuid().ToString(),
@@ -75,45 +74,10 @@ namespace MeAnotoApi.Controllers {
 			if (user is not null) {
 				var result = await this._UserManager.CreateAsync(user, model.Password);
 				return !result.Succeeded
-					? this.BadRequest(new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." })
+					? this.BadRequest(this.BadRequest(new Response { Status = "Error", Message = "Creation failed" }))
 					: this.Ok(new Response { Status = "Ok", Message = "Created successfully" });
 			}
 			return this.Unauthorized();
-		}
-		[HttpPost]
-		[Route("Register/Root")]
-		public async Task<IActionResult> RegisterRoot([FromBody] RootRegisterModel model) {
-			var userExists = await this._UserManager.FindByNameAsync(model.UserName);
-			if (userExists != null) {
-				return this.Unauthorized();
-			}
-			User user = model.Role switch {
-				"Administrator" => new AttendeeUser() {
-					Email = model.Email,
-					SecurityStamp = Guid.NewGuid().ToString(),
-					UserName = model.UserName
-				},
-				"Manager" => new ProfessorUser() {
-					Email = model.Email,
-					SecurityStamp = Guid.NewGuid().ToString(),
-					UserName = model.UserName
-				},
-				_ => null,
-			};
-			var result = await this._UserManager.CreateAsync(user, model.Password);
-			if (!result.Succeeded) {
-				return this.BadRequest(new Response { Status = "Error", Message = "User creation failed! Please check user details and try again." });
-			}
-			if (!await this._RoleManager.RoleExistsAsync(UserRoles.Root)) {
-				_ = await this._RoleManager.CreateAsync(new IdentityRole(UserRoles.Root));
-			}
-			if (!await this._RoleManager.RoleExistsAsync(UserRoles.Default)) {
-				_ = await this._RoleManager.CreateAsync(new IdentityRole(UserRoles.Default));
-			}
-			if (await this._RoleManager.RoleExistsAsync(UserRoles.Root)) {
-				_ = await this._UserManager.AddToRoleAsync(user, UserRoles.Root);
-			}
-			return this.Ok(new Response { Status = "Ok", Message = "Created successfully" });
 		}
 	}
 }
