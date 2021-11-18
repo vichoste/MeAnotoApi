bc191cff8b3f39a1faf2e041cdbf3d57628a8240
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-11-17 21:38:21 -0300

    Dev init

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace MeAnotoApi;
-
+//
 public class Program {
 	public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 	public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => _ = webBuilder.UseStartup<Startup>());
